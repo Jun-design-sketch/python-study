43e974e88d96fa4538b5ba9c50a48d9bb24bfb01
jeong <jeong.junhueck@gmail.com>
2026-06-28 15:13:57 +0900

    join

--- 260625/join.py
+++ 260625/join.py
@@ -5,4 +5,15 @@ import pandas as pd
 BASE_DIR = Path(__file__).resolve().parent
 EMPLOYEE_PATH = BASE_DIR / "input/emp.csv"
 HOBBIE_PATH = BASE_DIR / "input/hob.csv"
+OUTPUT_PATH = BASE_DIR / "output"
+OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
 
+emp = pd.read_csv(EMPLOYEE_PATH)
+hob = pd.read_csv(HOBBIE_PATH)
+
+result = pd.merge(emp, hob, "left", "취미이름")
+print(result)
+
+
+output_file = OUTPUT_PATH / "left_joined.csv"
+result.to_csv(output_file, index=False)
